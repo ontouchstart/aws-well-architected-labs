56b2e009572c0b6b4551965f3b85676d87464373
Sam Liu <ontouchstart@gmail.com>
2022-04-04 17:13:14 -0400

    cleanup walab

--- cli/ec2/instance-ids.py
+++
@@ -1,7 +0,0 @@
-import boto3
-ec2 = boto3.client('ec2')
-instances = ec2.describe_instances()
-reservations = instances['Reservations']
-for r in reservations:
-	for i in r['Instances']:
-		print(i['InstanceId'])
