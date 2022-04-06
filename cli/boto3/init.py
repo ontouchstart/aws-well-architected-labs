c1734c739cad8387fd50684f8646f60b0b548707
Sam Liu <ontouchstart@gmail.com>
2022-04-06 15:36:07 -0400

    print(json.dumps(vpcs, indent=2, default=str))

--- cli/boto3/init.py
+++ cli/boto3/init.py
@@ -1,17 +1,19 @@
 import boto3
 import json
-iam = boto3.client('iam')
-s3 = boto3.client('s3')
-cf = boto3.client('cloudformation')
-ec2 = boto3.client('ec2')
+iam_client = boto3.client('iam')
+s3_client = boto3.client('s3')
+cf_client = boto3.client('cloudformation')
+ec2_client = boto3.client('ec2')
 
-user = iam.get_user()
-users = iam.list_users()
-buckets = s3.list_buckets()
-stacks = cf.list_stacks()
-instances = ec2.describe_instances()
+user = iam_client.get_user()
+users = iam_client.list_users()
+buckets = s3_client.list_buckets()
+stacks = cf_client.list_stacks()
+instances = ec2_client.describe_instances()
+vpcs = ec2_client.describe_vpcs()
 
 print(dir())
 print(json.dumps(user, indent=2, default=str))
 print(json.dumps(instances, indent=2, default=str))
 print(json.dumps(stacks, indent=2, default=str))
+print(json.dumps(vpcs, indent=2, default=str))
